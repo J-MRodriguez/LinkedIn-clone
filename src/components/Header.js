761796eaadf0d564b30fb15b57cf392b39700c7c
J-MRodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2021-09-01 22:31:07 -0300

    starting home

--- src/components/Header.js
+++ src/components/Header.js
@@ -197,6 +197,19 @@ const NavList = styled.li`
         }
     }
 `
+const SignOut = styled.div`
+    position: absolute;
+    top: 45px;
+    background: white;
+    border-radius: 0 0 5px 5px;
+    width: 100px;
+    height: 40px;
+    font-size: 16px;
+    transition-duration: 167ms;
+    text-align: center;
+    display: none;
+`
+
 const User = styled(NavList)`
     a > svg{
         width: 20px;
@@ -211,18 +224,16 @@ const User = styled(NavList)`
         display: flex;
         align-items: center;
     }
+
+    &:hover{
+        ${SignOut}{
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+    }
+
 `
 const Work = styled(User)`
     border-left: 1px solid rgba(0,0,0,0.08);
-`
-const SignOut = styled.div`
-    position: absolute;
-    top: 45px;
-    background: white;
-    border-radius: 0 0 5px 5px;
-    width: 100px;
-    height: 40px;
-    font-size: 16px;
-    transition-duration: 167ms;
-    text-align: center;
 `
