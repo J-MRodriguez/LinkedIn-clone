a9aa8b72fece434c6defb5259545a9bbc7d94786
J-MRodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2021-09-04 17:34:06 -0300

    firebase installed

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQDFf7pWK2F5FYqSuIvTTqCEpooEo6V94",
+  authDomain: "linkedin-clone-cae8f.firebaseapp.com",
+  projectId: "linkedin-clone-cae8f",
+  storageBucket: "linkedin-clone-cae8f.appspot.com",
+  messagingSenderId: "578309418692",
+  appId: "1:578309418692:web:06b193907bad7efbdfe391",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = app.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage;
+
+export { auth, provider, storage };
+export default db;
