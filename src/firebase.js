9569867d97d3536fc598ac62acd489f268c16540
Juanma Rodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2021-10-14 22:43:16 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCQDFf7pWK2F5FYqSuIvTTqCEpooEo6V94",
-  authDomain: "linkedin-clone-cae8f.firebaseapp.com",
-  projectId: "linkedin-clone-cae8f",
-  storageBucket: "linkedin-clone-cae8f.appspot.com",
-  messagingSenderId: "578309418692",
-  appId: "1:578309418692:web:06b193907bad7efbdfe391",
+  apiKey: "AIzaSyAzxLbX-rD2cLVrp1a3YUk5R4C8RBIRADY",
+  authDomain: "linkedin-clone2-5b32c.firebaseapp.com",
+  projectId: "linkedin-clone2-5b32c",
+  storageBucket: "linkedin-clone2-5b32c.appspot.com",
+  messagingSenderId: "887942585824",
+  appId: "1:887942585824:web:ee39977318a9da0d311f85",
 };
 
 const app = firebase.initializeApp(firebaseConfig);
